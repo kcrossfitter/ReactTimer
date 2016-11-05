5ed79205196a7dd1a01d956efc04b76333ff5766
Sangwook Cho <korean.crossfitter@gmail.com>
2016-11-05 15:47:14 +0900

    Add Countdown functionality

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -7,12 +7,37 @@ class Countdown extends Component {
   constructor(props) {
     super(props);
     this.handleSetCountdown = this.handleSetCountdown.bind(this);
-    this.state = {count: 0};
+    this.state = {
+      count: 0,
+      countDownStatus: 'stopped'
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.countDownStatus !== prevState.countDownStatus) {
+      switch(this.state.countDownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+        case 'stopped':
+          break;
+      }
+    }
+  }
+
+  startTimer() {
+    this.timer = setInterval(() => {
+      var newCount = this.state.count - 1;
+      this.setState({
+        count: newCount > 0 ? newCount : 0 
+      });
+    }, 1000);
   }
 
   handleSetCountdown(seconds) {
     this.setState({
-      count: seconds
+      count: seconds,
+      countDownStatus: 'started'
     });
   }
 
