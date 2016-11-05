5b62a08b20aedf4465fb25debe3b2cbea06aad16
Sangwook Cho <korean.crossfitter@gmail.com>
2016-11-05 13:41:37 +0900

    Add CountdownForm component and test

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,16 +1,27 @@
 import React, {Component} from 'react';
 
 import Clock from 'Clock';
+import CountdownForm from 'CountdownForm';
 
 class Countdown extends Component {
   constructor(props) {
     super(props);
+    this.handleSetCountdown = this.handleSetCountdown.bind(this);
+    this.state = {count: 0};
+  }
+
+  handleSetCountdown(seconds) {
+    this.setState({
+      count: seconds
+    });
   }
 
   render() {
+    var {count} = this.state;
     return (
       <div>
-        <Clock totalSeconds={129} />
+        <Clock totalSeconds={count} />
+        <CountdownForm onSetCountdown={this.handleSetCountdown}/>
       </div>
     );
   }
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,10 +5,9 @@ import Navigation from 'Navigation';
 const Main = (props) => {
   return (
     <div>
-      <div>
-        <Navigation />
-        <div>
-          <p>Main.jsx rendered</p>
+      <Navigation />
+      <div className="row">
+        <div className="column small-centered medium-6 large-4">
           {props.children}
         </div>
       </div>
